f0cbb2c7fe7ef6e2e7248712823ee775eda65420
UI369 <UI369x@gmail.com>
2023-06-04 21:13:06 -0700

    basic layout

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,45 +1,84 @@
 // pages/index.js
-import styled from 'styled-components';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f2f2f2;
-`;
-
-const Title = styled.h1`
-  font-size: 3rem;
-  color: #333;
-  margin-bottom: 2rem;
-`;
-
-const Subtitle = styled.h2`
-  font-size: 1.5rem;
-  color: #666;
-  margin-bottom: 4rem;
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-size: 1.2rem;
-  background-color: #333;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
-const LandingPage = () => {
+const LandingPage: NextPage = () => {
   return (
-    <Container>
-      <div>
-        <Title>Welcome to My Web3 Site</Title>
-        <Subtitle>Explore the possibilities of Web3 technology.</Subtitle>
-        <Button>Get Started</Button>
-      </div>
-    </Container>
+    <div className={styles.container}>
+      <Head>
+        <title>DAO Masons</title>
+        <meta
+          content="DAO Masons is a service dao and designs dao mechanisms inspired by games"
+          name="description"
+        />
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
+
+      <main className={styles.main}>
+        <ConnectButton />
+
+        <h1 className={styles.title}>
+          Welcome to <a href="">RainbowKittt</a> + <a href="">wagmi</a> +{' '}
+          <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Getttt started by editing{' '}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a className={styles.card} href="https://rainbowkit.com">
+            <h2>RainbowKit Documentation &rarr;</h2>
+            <p>Learn how to customize your wallet connection flow.</p>
+          </a>
+
+          <a className={styles.card} href="https://wagmi.sh">
+            <h2>wagmi Documentation &rarr;</h2>
+            <p>Learn how to interact with Ethereum.</p>
+          </a>
+
+          <a
+            className={styles.card}
+            href="https://github.com/rainbow-me/rainbowkit/tree/main/examples"
+          >
+            <h2>RainbowKit Examples &rarr;</h2>
+            <p>Discover boilerplate example RainbowKit projects.</p>
+          </a>
+
+          <a className={styles.card} href="https://nextjs.org/docs">
+            <h2>Next.js Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a
+            className={styles.card}
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+          >
+            <h2>Next.js Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            className={styles.card}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
+          DAO Masons 2023
+        </a>
+      </footer>
+    </div>
   );
 };
 
